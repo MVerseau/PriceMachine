abd063cf2cd2f82d4aa8874371c0fcfe13ea6bf5
Mverseau <mverseau@gmail.com>
2024-12-07 21:36:55 +0300

    first commit

--- project.py
+++ project.py
@@ -1,7 +1,8 @@
 import os
-import json
 import csv
 import sys
+
+'''Использованы только проприетарные библиотеки.'''
 
 
 class PriceMachine():
@@ -19,11 +20,11 @@ class PriceMachine():
         for files in os.walk(file_path):
             for file in files[-1]:
                 if 'price' in file and file.endswith('.csv'):
-                    # print(file)
                     with open(file, 'r', encoding='utf-8') as f:
                         rows = csv.DictReader(f)
+                        # Приведение к единому формату {Наименование: наименование, Цена: цена, Вес: вес}
                         for row in rows:
-                            row = {k: v for k, v in row.items() if k in (good + price + weight)}
+                            row = {k: v.lower() for k, v in row.items() if k in (good + price + weight)}
                             for i in fields:
                                 for j in fields[i]:
                                     if j in row.keys():
@@ -33,31 +34,14 @@ class PriceMachine():
             self.name_length = len(max(self.data, key=lambda name: len(name['Наименование']))['Наименование'])
             self.data.sort(key=lambda x: float(x['Цена']) / float(x['Вес']))
             return self.data
-        '''
-            Сканирует указанный каталог. Ищет файлы со словом price в названии.
-            В файле ищет столбцы с названием товара, ценой и весом.
-            Допустимые названия для столбца с товаром:
-                товар
-                название
-                наименование
-                продукт
-                
-            Допустимые названия для столбца с ценой:
-                розница
-                цена
-                
-            Допустимые названия для столбца с весом (в кг.)
-                вес
-                масса
-                фасовка
-        '''
 
-    def _search_product_price_weight(self, headers):
-        '''
-            Возвращает номера столбцов
-        '''
+    # Заготовка не использована, т.к. в работе использован словарь
+    # def _search_product_price_weight(self, headers):
+    #     '''
+    #         Возвращает номера столбцов
+    #     '''
 
-    def export_to_html(self, fname='output1.html'):
+    def export_to_html(self, fname='output.html'):
         result = '''
         <!DOCTYPE html>
         <html>
@@ -92,7 +76,7 @@ class PriceMachine():
     def find_text(self, text):
         data = []
         for item in self.data:
-            if text in item['Наименование'].lower():
+            if text.lower() in item['Наименование'].lower():
                 data.append(item)
         if len(data) == 0:
             print('Ничего не найдено.')
@@ -106,24 +90,28 @@ class PriceMachine():
             f'{'Цена за кг.'.center(9, ' ')}'
         )
 
-        for i in range(1, len(data)):
+        for i in range(len(data)):
             print(
-                f'{str(i).ljust(5, " ")}'
+                f'{str(i + 1).ljust(5, " ")}'
                 f'{data[i]['Наименование'].ljust(self.name_length, " ")}'
                 f'{data[i]['Цена'].rjust(6, " ")}'
                 f'{data[i]['Вес'].rjust(3, " ")}'
                 f'{data[i]["file_name"].center(13, " ")}'
                 f'{str(round(float(data[i]['Цена']) / float(data[i]['Вес']), 2)).ljust(9, ' ')}'
             )
+        print('\n'+'Введите слово или его часть для поиска продукта либо exit для выхода из программы: ')
 
 
 pm = PriceMachine()
 print(pm.load_prices())
 
-for line in sys.stdin:
-    if 'exit' in line[:-1].lower():
-        print('the end')
-        sys.exit()
-    pm.find_text(line[:-1].lower())
-
-print(pm.export_to_html())
+if __name__ == '__main__':
+    print('\n'+'Введите слово или его часть для поиска продукта либо exit для выхода из программы: ')
+    for line in sys.stdin:
+        if 'exit' in line[:-1].lower():
+            print('\n' + 'the end' + '\n')
+            to_html = input('Перенести данные в HTML? Д/Н ')
+            if to_html.lower() in 'lfда':
+                print(pm.export_to_html())
+            sys.exit()
+        pm.find_text(line[:-1].lower())
